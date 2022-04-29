087652d3687fa34e5996edcda0b0c8c0fe90c6ff
j0lama <jon_larrea@hotmail.com>
2022-04-29 16:12:57 +0100

    Clean up. TODO: Automate IP configuration and modify proxy

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # kFAPI
+
+OAI-nFAPI deployment on Kubernetes
+
+#### Build Docker image
+```bash
+docker build -t j0lama/<image name>:latest .
+```
+
+#### Push Docker image to DockerHub
+```bash
+docker image push j0lama/<image name>:latest
+```
+
+#### Deploy Pod
+```bash
+kubectl run <pod name> --rm -i --tty --privileged --image j0lama/<image name>:latest -- bash
+```
+
+#### Run bash inside pod
+```bash
+kubectl exec -ti <pod name> -- bash
+```
--- docker/core/run_core.sh
+++ docker/core/run_core.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ ! -f core-configured ]; then
+    echo "Configuring core..."
+    # Configure the network
+    sysctl -w net.ipv4.ip_forward=1
+    iptables -t nat -A POSTROUTING -s 1.1.0.0/16 ! -o srsTUN -j MASQUERADE
+    CORE_IP_ADDR=$(ifconfig eth0 | grep 'inet' | awk '{ print $2}')
+    sed -i "s/core_addr/$CORE_IP_ADDR/g" core.conf
+    touch core-configured
+    echo "Configuration done!"
+fi
 
-srsepc_if_masq.sh eth0
-
-CORE_IP_ADDR=$(ifconfig eth0 | grep 'inet' | awk '{ print $2}')
-sed -i "s/enb_addr/$CORE_IP_ADDR/g" core.conf
-
-srsepc epc.conf
+srsepc core.conf
